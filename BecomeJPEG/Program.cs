7180468b720e0f0c58207eea6e5b699fa0ed57cb
Toreole <toreole.official@gmail.com>
2022-09-07 03:32:01 +0200

    It is done

--- BecomeJPEG/Program.cs
+++ BecomeJPEG/Program.cs
@@ -2,6 +2,10 @@
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace BecomeJPEG
 {
@@ -11,19 +15,38 @@ namespace BecomeJPEG
         {
             string windowName = "first window";
             CvInvoke.NamedWindow(windowName);
+          
+            //create the videocapture. this will default to the first installed device.
+            VideoCapture capture = new VideoCapture();
 
-            Mat img = new Mat(200, 400, DepthType.Cv8U, 3);
-            img.SetTo(new Bgr(255, 0, 0).MCvScalar);
+            //subscribe to the ImageGrabbed event.
+            capture.ImageGrabbed += new EventHandler(delegate (object sender, EventArgs e)
+            {
+                Mat frame = new Mat(capture.Width, capture.Height, DepthType.Default, 3);
+                capture.Retrieve(frame);
+                
 
-            CvInvoke.PutText(
-                img,
-                "Hello World",
-                new System.Drawing.Point(10, 80), FontFace.HersheyComplex,
-                1.0,
-                new Bgr(0, 255, 0).MCvScalar);
+                Image<Bgr, byte> img = frame.ToImage<Bgr, byte>();
 
-            CvInvoke.Imshow(windowName, img);
+                //jpeg compress the hell out of it.
+                byte[] data = img.ToJpegData(0);
+                ////dispose the old image data.
+                //img.Dispose();
+
+                CvInvoke.Imdecode(data, ImreadModes.Color, frame);
+                //show the image.
+                CvInvoke.Imshow(windowName, frame);
+
+                img.Dispose();
+                frame.Dispose();
+            });
+
+            //start the image grabbing thread.
+            capture.Start();
+
             CvInvoke.WaitKey(0);
+            //stop the image grabbing thread.
+            capture.Stop();
             CvInvoke.DestroyWindow(windowName);
         }
     }
