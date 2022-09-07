f6b47ad33867798d4b146aeadcb5bce1beca00d1
Toreole <toreole.official@gmail.com>
2022-09-08 00:08:02 +0200

    Comments, clean up

--- BecomeJPEG/Program.cs
+++ BecomeJPEG/Program.cs
@@ -2,19 +2,19 @@
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace BecomeJPEG
 {
     internal class Program
     {
+        //settings and and a Random instance that should just be accessible.
         static Random rng = new Random();
         static volatile float frameDropChance = 0.85f;
         static volatile int compressionQuality = 0;
+        //readonly windowName.
+        static readonly string windowName = "BecomeJPEG Preview";
+        //Property that automatically clamps the quality between 0 and 100.
         static int CompressionQuality 
         { 
             get => compressionQuality; 
@@ -24,11 +24,11 @@ namespace BecomeJPEG
                     (value > 100) ? 100 
                     : value; 
             } }
-        static bool quit = false;
 
+        //entry point. you know how it is.
         static void Main(string[] args)
         {
-            string windowName = "first window";
+            //create the window.
             CvInvoke.NamedWindow(windowName);
           
             //create the videocapture. this will default to the first installed device.
@@ -43,6 +43,7 @@ namespace BecomeJPEG
             //subscribe to the ImageGrabbed event. This will follow the device's framerate.
             capture.ImageGrabbed += new EventHandler(delegate (object sender, EventArgs e)
             {
+                //chance for frame to be dropped is checked before anything else.
                 if (rng.NextDouble() <= frameDropChance)
                     return;
                 //retrieve the grabbed frame data.
@@ -61,49 +62,56 @@ namespace BecomeJPEG
             //start the image grabbing thread.
             capture.Start();
 
+            //input commands are handled on a seperate thread for ease of use.
             Thread commandThread = new Thread(DoConsoleCommands);
             commandThread.Start();
 
+            //when the commandThread stops, thats a sign that the "quit" command has been used.
             while (commandThread.IsAlive)
             {
                 CvInvoke.WaitKey(1);
             }
 
-            //stop the image grabbing thread.
+            //clean up anything left to clean up.
             capture.Stop();
+            frame.Dispose();
+            frameMatrix.Dispose();
             CvInvoke.DestroyWindow(windowName);
         }
 
         //this is a seperate thread.
         private static void DoConsoleCommands()
         {
+            //receive input into these two local variables.
             string input = "";
             string[] inputArgs;
+            //only exit point is the "quit" command. 
             while(true)
             {
+                //read a line from the console.
                 input = Console.ReadLine();
                 if (input == null)
                     continue;
+                //split the line into individual arguments
                 inputArgs = input.Split(' ');
 
-                if (inputArgs.Length <= 0)
+                //shouldnt do anything if input is empty or first argument is blank.
+                if (inputArgs.Length <= 0 || string.IsNullOrWhiteSpace(inputArgs[0]))
                     continue;
 
-                if (string.IsNullOrWhiteSpace(inputArgs[0]))
-                    continue;
-
+                //quit command is checked first.
                 if (inputArgs[0].ToLower() == "quit")
                 {
-                    quit = true;
                     return;
                 }
 
+                //set command is next - it requires 3 params.
                 if (inputArgs[0].ToLower() == "set" && inputArgs.Length == 3)
                 {
                     if (inputArgs[1] == "droprate")
                         frameDropChance = float.Parse(inputArgs[2]) / 100f; //divide by 100, so 100 means 100%, 10 means 10%, etc.
                     if (inputArgs[1] == "quality")
-                        CompressionQuality = int.Parse(inputArgs[2]);
+                        CompressionQuality = int.Parse(inputArgs[2]); //can be set directly to Property because it handles Clamping.
                 }
             }
         }
